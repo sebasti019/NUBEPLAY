118f77b76716886d41b3b1ecf0cd7e34bc365b4e
Your Name <you@example.com>
2025-05-17 09:48:59 -0400

    cambio cosmetico

--- NUBEPLAY/src/main/java/com/example/NUBEPLAY/NubeplayApplication.java
+++ NUBEPLAY/src/main/java/com/example/NUBEPLAY/NubeplayApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NubeplayApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(NubeplayApplication.class, args);
 	}
+
 
 }
