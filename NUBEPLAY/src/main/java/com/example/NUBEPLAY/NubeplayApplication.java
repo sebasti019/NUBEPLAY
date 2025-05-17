b30d33cca7307f786fcede2ec79afc647bda021a
Your Name <you@example.com>
2025-05-17 09:46:09 -0400

    1er commit

---
+++ NUBEPLAY/src/main/java/com/example/NUBEPLAY/NubeplayApplication.java
@@ -0,0 +1,13 @@
+package com.example.NUBEPLAY;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NubeplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NubeplayApplication.class, args);
+	}
+
+}
